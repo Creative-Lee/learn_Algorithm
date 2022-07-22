6cd29b4e447df603ac591b0e3f364010ebb0035f
Dohyeon Lee_Laptop <dlehgus1110@naver.com>
2022-07-22 19:03:06 +0900

    다시 시작

--- realSolve.js
+++ realSolve.js
@@ -1,24 +1,29 @@
-const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().trim().split('\n').map(e=>+e)
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => +e);
 
-const testcaseArr = input.slice(1)
+const testcaseArr = input.slice(1);
 
-const Q = 25
-const D = 10
-const N = 5
-const P = 1
+const Q = 25;
+const D = 10;
+const N = 5;
+const P = 1;
 
-let answer = ''
+let answer = "";
 
-for(let i = 0; i < testcaseArr.length; i++) {
-  let change = testcaseArr[i]
+for (let i = 0; i < testcaseArr.length; i++) {
+  let change = testcaseArr[i];
 
-  let Qn = Math.floor(change / Q) 
-  let Dn = Math.floor(change % Q / D)
-  let Nn = Math.floor(change % Q % D / N)
-  let Pn = Math.floor(change % Q % D % N / P) 
-  
-  answer += `${Qn} ${Dn} ${Nn} ${Pn}\n`
+  let Qn = Math.floor(change / Q);
+  let Dn = Math.floor((change % Q) / D);
+  let Nn = Math.floor(((change % Q) % D) / N);
+  let Pn = Math.floor((((change % Q) % D) % N) / P);
+
+  answer += `${Qn} ${Dn} ${Nn} ${Pn}\n`;
 }
 
-console.log(answer)
+console.log(answer);
