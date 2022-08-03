eff1470d28170b7e4f1878a63207eacc9bd728b7
Dohyeon Lee <dlehgus1110@naver.com>
2022-08-03 19:42:46 +0900

    경문 추천문제 도전중

--- solve.js
+++ solve.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(file).toString().trim();
+let [matrix, ...value] = fs.readFileSync(file).toString().trim().split("\n");
+matrix = matrix.split(" ").map((e) => +e);
+let [N, M] = matrix;
+
+value = value.map(e=>e)
 
 function solution() {
-  while (input.includes("XX")) {
-    input = input.replace("XX", "BB");
-  }
-  while (input.includes("BBBB")) {
-    input = input.replace("BBBB", "AAAA");
-  }
-  if (input.includes("X")) {
-    console.log(-1);
-    return;
-  }
-  console.log(input);
+  let stack = [];
 }
 
 solution();
