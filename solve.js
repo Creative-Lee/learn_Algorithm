dc23d82a359deb1b9e4d7d791c6ab9aee1df7cde
Dohyeon Lee <dlehgus1110@naver.com>
2022-08-24 17:40:29 +0900

    review complete: 17298

--- solve.js
+++ solve.js
@@ -1,68 +1,23 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let [sc, ss, stuC, ...stus] = fs.readFileSync(file).toString().trim().split("\n");
+let [tcc, tcs] = fs.readFileSync(file).toString().trim().split("\n");
 
-sc = +sc;
-let switches = ss
+tcc = +tcc;
+tcs = tcs
   .trim()
   .split(" ")
-  .map((e, idx) => {
-    let isOn = e === "1";
-    return [idx + 1, isOn];
-  }); //스위치 번호 = idx + 1 , 스위치 상태
-stuC = +stuC;
-stus = stus.map((e) => e.trim());
+  .map((e) => +e);
 
 function solution() {
-  for (let i = 0; i < stuC; i++) {
-    let [sex, num] = stus.shift().split(" ");
-    sex = +sex;
-    num = +num;
-
-    if (sex === 1) {
-      switches = switches.map((arr) => {
-        let isMulti = arr[0] % num === 0;
-        if (isMulti) {
-          return [arr[0], !arr[1]];
-        }
-        return arr;
-      });
-    } else {
-      let left = num - 1 - 1;
-      let right = num - 1 + 1;
-
-      switches[num - 1][1] = !switches[num - 1][1];
-
-      while (true) {
-        if (left === -1) break;
-        if (right === sc) break;
-        if (switches[left][1] !== switches[right][1]) break;
-
-        if (switches[left][1] === switches[right][1]) {
-          switches[left][1] = !switches[left][1];
-          switches[right][1] = !switches[right][1];
-        }
-        left--;
-        right++;
-      }
+  let stack = [];
+  let result = new Array(tcc).fill(-1);
+  for (let i = 0; i < tcc; i++) {
+    while (stack.length && stack[stack.length - 1][0] < tcs[i]) {
+      let idx = stack.pop()[1];
+      result[idx] = tcs[i];
     }
+    stack.push([tcs[i], i]);
   }
-
-  let sState = switches.map((arr) => (arr[1] === true ? "1" : "0"));
-  let result = [];
-  for (i = 0; i < sState.length; i++) {
-    if (i !== 0 && i % 20 === 19) {
-      result.push(sState[i]);
-      result.push("\n");
-      continue;
-    }
-    if (i === sState.length - 1) {
-      result.push(sState[i]);
-      continue;
-    }
-    result.push(sState[i]);
-    result.push(" ");
-  }
-  console.log(result.join(""));
+  console.log(result.join(" "));
 }
 solution();
